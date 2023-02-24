7f5b0043cd040cb7a1ce3082e3b701754274454b
Lynn-Mei <hugo.murura@gmail.com>
2023-02-24 16:10:27 +0100

    Implementation de filieres dans la indexView

--- js/view/viewindex.js
+++ js/view/viewindex.js
@@ -1,6 +1,34 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 class ViewIndex {
     constructor(controler) {
         this.controler = controler;
+        this.filieres = document.getElementById("years");
+        this.listFilieres();
+    }
+    listFilieres() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                let dao = new FiliereDao();
+                let filieres = yield dao.getAll();
+                filieres.forEach((filiere) => {
+                    let item = document.createElement("option");
+                    item.value = filiere.ID.toString();
+                    item.innerHTML = filiere.nom;
+                    this.filieres.appendChild(item);
+                });
+            }
+            catch (e) {
+                alert(e.message);
+            }
+        });
     }
 }
 //# sourceMappingURL=viewindex.js.map
--- js/view/viewindex.ts
+++ js/view/viewindex.ts
@@ -1,7 +1,27 @@
 class ViewIndex implements IObserver {
   private controler: Controler;
+  private filieres: HTMLSelectElement;
 
   public constructor(controler: Controler) {
     this.controler = controler;
+    this.filieres = document.getElementById("years") as HTMLSelectElement;
+    this.listFilieres();
+  }
+
+  private async listFilieres() {
+    try {
+      let dao = new FiliereDao();
+      let filieres = await dao.getAll();
+      filieres.forEach((filiere: Filiere) => {
+        let item: HTMLOptionElement = document.createElement("option") as
+          HTMLOptionElement;
+        item.value = filiere.ID.toString();
+        item.innerHTML = filiere.nom;
+        this.filieres.appendChild(item);
+      });
+    }
+    catch (e) {
+      alert(e.message);
+    }
   }
 }
